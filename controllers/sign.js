f2443c228d1b1511aafbd0d342680a606bd5fa14
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-06-14 21:01:48 +0530

    signin after authentication

--- controllers/sign.js
+++ controllers/sign.js
@@ -11,28 +11,26 @@ function isStringEmpty(str){
 
 exports.postSignup = async(req,res,next)=>{
     try{
-        const name = req.body.myname;
-        const email = req.body.myemail;
-        const passward = req.body.mypassward;
+        const name = req.body.name;
+        const email = req.body.email;
+        const passward = req.body.passward;
         const users = await SignUp.findAll();
         for(let i=0;i<users.length;i++){
             if(users[i].email==email){
-                return res.status(400).json({error:"User Already Exist!"});
+                return res.status(400).json({message:"User Already Exist!",success:false});
             }
         }
         if(isStringEmpty(name)||isStringEmpty(email)||isStringEmpty(passward)){
-            return res.status(400).json({error:"All Fields Are Mandatory"});
+            return res.status(400).json({message:"All Fields Are Mandatory",success:false});
         }
         const saltrounds=10;
         bcrypt.hash(passward,saltrounds,async(err,hash)=>{
             await SignUp.create({name:name,email:email,passward:hash});
-            res.status(201).json({message:"Successfully created New User"});
+            res.status(201).json({message:"Successfully created New User",success:true});
         })
     }catch(err){
         console.log(err);
-        res.status(500).json({
-            error:err
-        })
+        res.status(500).json({message:err,success:false})
     }
 }
 
