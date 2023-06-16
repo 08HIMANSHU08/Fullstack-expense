de402287a0317af7ec2876ec53e5401485c32979
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-06-16 12:36:47 +0530

    optimized code for premium features using sql

--- controllers/premiumfeature.js
+++ controllers/premiumfeature.js
@@ -6,24 +6,40 @@ const sequelize = require('../util/database');
 exports.getLeaderBoard = async(req,res,next)=>{
     try{
         // console.log("check");
-        const signup = await SignUp.findAll();
-        const expenses = await Expense.findAll();
-        const userAggregatedExpense = {}
-        expenses.forEach((expense) => {
-            // console.log(expense.amount);
-            if(userAggregatedExpense[expense.signupId]){
-                userAggregatedExpense[expense.signupId] += expense.amount;
-            }else{
-                    userAggregatedExpense[expense.signupId] = expense.amount;
-            } 
-        });
-        let userleaderBoardDetails = [];
-        signup.forEach((sign)=>{
-            userleaderBoardDetails.push({name:sign.name,total_expense:userAggregatedExpense[sign.id]||0})
-        })
-        console.log(userleaderBoardDetails);
-        userleaderBoardDetails.sort((a,b)=>b.total_expense-a.total_expense);
-        res.status(200).json(userleaderBoardDetails);
+        const userAggregatedExpense = await SignUp.findAll({
+            attributes:['id','name',[sequelize.fn('sum',sequelize.col('expenses.amount')),'total_expense']],
+            include:[
+                {
+                model:Expense,
+                attributes:[]
+                }
+            ],
+            group:['signup.id'],
+            order:[['total_expense','DESC']],
+         });
+         console.log("sdmhfgkjdfghdfkjhyerkjtghery",userAggregatedExpense);
+         res.status(200).json(userAggregatedExpense);
+        // const userAggregatedExpense = await Expense.findAll({
+        //     attributes:['signupId',[sequelize.fn('sum',sequelize.col('amount')),'total_expense']],
+        //     group:['signupId']
+        // });
+        // const userAggregatedExpense = {}
+        // expenses.forEach((expense) => {
+        //     // console.log(expense.amount);
+        //     if(userAggregatedExpense[expense.signupId]){
+        //         userAggregatedExpense[expense.signupId] += expense.amount;
+        //     }else{
+        //             userAggregatedExpense[expense.signupId] = expense.amount;
+        //     } 
+        // });
+        // console.log("sdmhfgkjdfghdfkjhyerkjtghery",userAggregatedExpense);
+        // let userleaderBoardDetails = [];
+        // signup.forEach((sign)=>{
+        //     userleaderBoardDetails.push({name:sign.name,total_expense:userAggregatedExpense[sign.id]||0})
+        // })
+        // console.log("sdmhfgkjdfghdfkjhyerkjtghery",userleaderBoardDetails);
+        // userleaderBoardDetails.sort((a,b)=>b.total_expense-a.total_expense);
+        
     }catch(err){
         console.log(err);
         res.status(500).json(err);
