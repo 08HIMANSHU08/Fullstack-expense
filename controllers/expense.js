51ab2413c68059686c61baefd665209ededffb23
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-06-16 15:15:01 +0530

    expense and premiumfeature file after optimisation

--- controllers/expense.js
+++ controllers/expense.js
@@ -1,10 +1,12 @@
 
 const Expense = require('../models/expensetable');
+const SignUp = require('../models/signup');
 
 exports.getExpense = async(req,res,next)=>{
     try{
         const users = await Expense.findAll({where:{signupId:req.signup.id}});
-        console.log(users);
+        // console.log(users);
+
         res.status(200).json({allExpense:users});
     }catch(err){
         console.log('get user is failed',JSON.stringify(err))
@@ -19,6 +21,8 @@ exports.postExpense = async(req,res,next)=>{
         const description = req.body.desc;
         const data = await Expense.create({amount:expense,description:description,category:category,signupId:req.signup.id});
         console.log(data);
+        const totalexp = Number(req.signup.totalexpense) + Number(expense);
+        await SignUp.update({totalexpense:totalexp},{where:{id:req.signup.id}});
         res.status(201).json({newExpense:data});
     }catch(err){
         console.log(err);
@@ -35,7 +39,10 @@ exports.deleteExpense = async(req,res,next)=>{
             return res.status(400).json({err:"ID is Missing"})
         }
         const userId = req.params.id;
+        const expdata = await Expense.findByPk(userId);
+        const totalexp = Number(req.signup.totalexpense) - Number(expdata.amount);
         await Expense.destroy({where:{id:userId,signupId:req.signup.id}});
+        await SignUp.update({totalexpense:totalexp},{where:{id:req.signup.id}});
         res.status(200)
     }
     catch(err){
--- controllers/premiumfeature.js
+++ controllers/premiumfeature.js
@@ -5,41 +5,11 @@ const sequelize = require('../util/database');
 
 exports.getLeaderBoard = async(req,res,next)=>{
     try{
-        // console.log("check");
         const userAggregatedExpense = await SignUp.findAll({
-            attributes:['id','name',[sequelize.fn('sum',sequelize.col('expenses.amount')),'total_expense']],
-            include:[
-                {
-                model:Expense,
-                attributes:[]
-                }
-            ],
-            group:['signup.id'],
-            order:[['total_expense','DESC']],
+            order:[['totalexpense','DESC']],
          });
-         console.log("sdmhfgkjdfghdfkjhyerkjtghery",userAggregatedExpense);
+        //  console.log("userAggregatedExpense",userAggregatedExpense)
          res.status(200).json(userAggregatedExpense);
-        // const userAggregatedExpense = await Expense.findAll({
-        //     attributes:['signupId',[sequelize.fn('sum',sequelize.col('amount')),'total_expense']],
-        //     group:['signupId']
-        // });
-        // const userAggregatedExpense = {}
-        // expenses.forEach((expense) => {
-        //     // console.log(expense.amount);
-        //     if(userAggregatedExpense[expense.signupId]){
-        //         userAggregatedExpense[expense.signupId] += expense.amount;
-        //     }else{
-        //             userAggregatedExpense[expense.signupId] = expense.amount;
-        //     } 
-        // });
-        // console.log("sdmhfgkjdfghdfkjhyerkjtghery",userAggregatedExpense);
-        // let userleaderBoardDetails = [];
-        // signup.forEach((sign)=>{
-        //     userleaderBoardDetails.push({name:sign.name,total_expense:userAggregatedExpense[sign.id]||0})
-        // })
-        // console.log("sdmhfgkjdfghdfkjhyerkjtghery",userleaderBoardDetails);
-        // userleaderBoardDetails.sort((a,b)=>b.total_expense-a.total_expense);
-        
     }catch(err){
         console.log(err);
         res.status(500).json(err);
