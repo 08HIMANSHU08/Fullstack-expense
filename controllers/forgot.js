230d698f67f36fb681cf702236b74688aa9027da
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-06-18 11:21:04 +0530

    pagination in backend

--- controllers/forgot.js
+++ controllers/forgot.js
@@ -1,8 +1,9 @@
 
 const SignUp = require('../models/signup');
-// const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const sequelize = require('../util/database');
+const ForgotPassward = require('../models/forgetpasswardrequest');
+// const jwt = require('jsonwebtoken');
+// const sequelize = require('../util/database');
+const {v4:uuidv4} = require('uuid');
 
 const Sib = require('sib-api-v3-sdk');
 require('dotenv').config()
@@ -10,38 +11,15 @@ const client = Sib.ApiClient.instance;
 const apiKey = client.authentications['api-key'];
 apiKey.apiKey = process.env.SENDMAIL_KEY
 
+const uuid = uuidv4();
 const tranEmailApi = new Sib.TransactionalEmailsApi();
-
-const sender = {
-    email:'himanshusinghpatel@gmail.com'
-}
-
-// const receivers = [
-//     {
-//         email:'phimanshu08@gmail.com'
-//     }
-// ]
-// tranEmailApi  
-//     .sendTransacEmail({
-//         sender,
-//         to:receivers,
-//         subject:'forget passward',
-//         TextContent: `forgetted the passward`
-//     })
-//     .then(console.log())
-//     .catch(console.log());
-
-// function generateAccessToken(id,name,ispremiumuser){
-//     console.log("purchase",id,name,ispremiumuser)
-//     return jwt.sign({signupId:id,name:name,ispremiumuser},process.env.TOKEN_SECRET);
-// }
-
    
     exports.postForgetPassword = async(req,res,next)=>{
         // const t = await sequelize.transaction();
+        // console.log("kcjewhfkj")
         try{
             const email = req.body.email;
-
+            // console.log(email,"enmfefhiuewyhiufyegf")
             const users = await SignUp.findAll({where:{email}});
             console.log(users,users[0])
             if(users[0].email==email){
@@ -53,19 +31,22 @@ const sender = {
                         email:email
                     }
                 ]
-                tranEmailApi  
+                await tranEmailApi  
                     .sendTransacEmail({
                         sender,
                         to:receivers,
-                        subject:'forget passward',
-                        TextContent: "Change your passward"
+                        subject:'Reset Your Passward',
+                        TextContent: `http://localhost:3000/password/resetpassword/${uuid}`
                     })
-                    .then(console.log())
-                    .catch(console.log());
+                // await t.commit();
+
+                await ForgotPassward.create({id:uuid,isactive:true,signupId:users[0].id});
+                // console.log("request send");
+                return res.status(200).json({message:"Clear",success:true});
             }
         }catch(err){
             // await t.rollback();
-            console.log(err);
+            console.log("err");
             res.status(500).json({message:err,success:false})
         }
     }
