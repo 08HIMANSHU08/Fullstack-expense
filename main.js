f5908bd9f697b7e4ad769f637f047d31b4c42cf4
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-06-18 15:22:27 +0530

    main backend file after helmet and morgan

--- main.js
+++ main.js
@@ -3,24 +3,29 @@
 const path = require('path');
 
 const express = require('express');
+const fs = require('fs');
 
 const bodyParser = require('body-parser');
-
-const errorController = require('./controllers/error');
-
 const sequelize=require('./util/database');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 var cors = require('cors');
 
 const SignUp = require('./models/signup');
 const Expense = require('./models/expensetable');
 const ForgotPassward = require('./models/forgetpasswardrequest');
+const urlModels = require('./models/urltable');
+
+const errorController = require('./controllers/error');
 
 const adminRoutes = require('./routes/admin');
 const expenseRoutes = require('./routes/expenseapp');
 const purchaseRoutes = require('./routes/purchase');
 const premiumFeatureRoutes = require('./routes/premiumfeature');
 const passwardRoutes = require('./routes/forgotpassward');
+require('dotenv').config();
+
 
 const Order = require('./models/order');
 const app = express();
@@ -33,6 +38,14 @@ app.set('views','views')
 app.use(bodyParser.json({extended:false}));
 
 app.use(express.static(path.join(__dirname,'public')));
+
+const accessLogStream = fs.createWriteStream(
+    path.join(__dirname,'access.log'),
+    {flags:'a'}
+);
+
+app.use(helmet());
+app.use(morgan('combined',{stream:accessLogStream}));
 
 app.use('/user',adminRoutes)
 
@@ -55,8 +68,11 @@ Order.belongsTo(SignUp);
 SignUp.hasMany(ForgotPassward);
 ForgotPassward.belongsTo(SignUp);
 
+SignUp.hasMany(urlModels);
+urlModels.belongsTo(SignUp);
+
 sequelize.sync()
 .then(result=>{
-    app.listen(3000)
+    app.listen(process.env.PORT)
 })
 .catch(err=>{console.log(err)});
